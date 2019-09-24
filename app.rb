127d3465248bd93a9fc970908b321864b3209556
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 06:57:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    "#{PigLatinizer.new(params[:text]).convert}"
+  end
 end
