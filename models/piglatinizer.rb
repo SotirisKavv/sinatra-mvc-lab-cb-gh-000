e623d795e2e7e8ef7e55ddf37bcdbc6447dcaaf7
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 07:08:42 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+
+  attr_reader :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def convert
+    if @text.match(/^[qwrtypsdfghjklzxcvbnm][aeiou]/)
+      text.push(text[0])
+      text.shift
+      text << "ay"
+    elsif @text.match(/^[qwrtypsdfghjklzxcvbnm]{2}/)
+      text.push(text[0])
+      text.push(text[1])
+      text.shift
+      text.shift
+      text << "ay"
+    else
+      text << "way"      
+    end
+  end
+  
+end
