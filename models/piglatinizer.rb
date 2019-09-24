764e1384a7a8139ab9c9625b24d8d827fc944af8
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 07:50:39 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,12 +9,12 @@ class PigLatinizer
   def convert
     if @text.match(/^[aeiou]/)
       text << "w"
-    elsif @text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 3
-      text = @text.slice(3..-1) + @text.slice(0,3)
-    elsif @text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 2
-      text = @text.slice(2..-1) + @text.slice(0,2)
-    elsif @text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 1
-      text = @text.slice(1..-1) + @text.slice(0,1)
+    elsif text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 3
+      text = text.slice(3..-1) + text.slice(0,3)
+    elsif text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 2
+      text = text.slice(2..-1) + text.slice(0,2)
+    elsif text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 1
+      text = text.slice(1..-1) + text.slice(0,1)
     end
     text << "ay"
   end
