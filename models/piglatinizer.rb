9bc7422ef7d8a816ccc46709e951123e18137909
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 07:47:32 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,9 +9,8 @@ class PigLatinizer
   def convert
     if @text.match(/^[aeiou]/)
       text << "way"
-    else
-      push = @text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count
-      text = @text.slice(push..-1) + @text.slice(0..push-1)
+    elsif @text.scan(/^[qwrtypsdfghjklzxcvbnm]*/i).count == 3
+      text = @text.slice(3..-1) + @text.slice(0,3)
       text.gsub(/^[qwrtypsdfghjklzxcvbnm]*/i, '') << "ay"
     end
   end
